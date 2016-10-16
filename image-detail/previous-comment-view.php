90fff56e718ea066ee15a6eef9763e959436ba47
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2016-10-15 22:17:29 -0400

    more changes

--- image-detail/previous-comment-view.php
+++ image-detail/previous-comment-view.php
@@ -1,5 +1,5 @@
 <div class="previous__container">
-    <h3 class="previous__author"><?php echo $commentOne->title?></h3>
-    <p><?php echo $commentOne->comment?></p>
-    <p class="image-user"><?php echo $date1->format('F d, Y h:ia')?></p>
+    <h3 class="previous__author"><span class="comment__title">User:</span></br> <?php echo $commentOne->author?></h3>
+    <p><span class="comment__title">Commented:</span></br> <?php echo $commentOne->comment?></p>
+    <p class="image-user"><span class="comment__title">On:</span></br> <?php echo $date1->format('F d, Y h:ia')?></p>
 </div>
